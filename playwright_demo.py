da64c23db1bc3bca9f6d927858fa39bc9380b13b
Jeffery <caopengwww321@gmail.com>
2024-12-26 01:48:50 +0800

    playwright sample demo

--- playwright_demo.py
+++ playwright_demo.py
@@ -1,2 +1,29 @@
-print("This is a test!")
-print("Hello World!")
+import playwright.sync_api
+
+# 启动 playwright driver 进程
+p = playwright.sync_api.sync_playwright().start()
+
+
+# 启动浏览器，返回 Browser 类型对象
+browser = p.chromium.launch(headless=False)
+
+# 创建新页面，返回 Page 类型对象
+page = browser.new_page()
+page.goto("https://www.byhy.net/cdn2/files/selenium/stock1.html")
+print(page.title()) # 打印网页标题栏
+
+# 输入通讯，点击查询。这是定位与操作，是自动化重点，后文详细讲解
+page.locator('#kw').fill('通讯')  # 输入通讯
+page.locator('#go').click()      # 点击查询
+page.wait_for_timeout(1000)
+# 打印所有搜索内容
+lcs = page.locator(".result-item").all()
+for lc in lcs:
+    print(lc.inner_text())
+
+
+# 关闭浏览器
+browser.close()
+
+# 关闭 playwright driver 进程
+p.stop()
